f1786a4c64b13950788b28e7a3551e28d3fec394
Ausmosian <39672173+Ausmosian@users.noreply.github.com>
2020-05-21 19:37:29 +0530

    Update random_input_generator.cpp
    Better Random_Input_Generator.cc

--- random_input_generator.cpp
+++ random_input_generator.cpp
@@ -1,60 +1,33 @@
-// This file generates random test cases
-// using the time(NULL) variable of UNIX style OS
-// Running it at diffrent times will create diffrent
-// results because of the dynamic seed value
-
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define max_range 100000
+#define endl '\n'
+struct _ { ios_base::Init i; _() { cin.sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL); } } _;
 
-int main()
+long long rand(long long a, long long b) {
+	return a + rand() % (b - a + 1);
+}
+
+int main(int argc, char const *argv[])
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.precision(10);
+	srand(atoi(argv[1]));
 
-    ll n,q;
-    // n is the number of data polls
-    // q is the number of queries
-    cin >> n >> q;
+	int n = rand(2, 20), q = rand(1, 20);
+	cout << n << " " << q << endl;
 
-    
-    srand(time(NULL));
-    // generating random data polls
-    cout << n << endl;
-    for (ll i=0;i<=n-1;i++)
-    {
-        ll temp=(rand()%max_range)+1;
-        cout << temp << " ";
-    }
-    cout << endl;
+	for (int i = 0; i <= n - 1; i++) cout << rand(1, 100) << " ";
+	cout << endl;
 
+	while (q--) {
+		int type = rand(1, 2); cout << type << " ";
 
-    cout << q << endl;
-    // genrerating queries
-    // id=1 implies update query
-    // id=2 implies range query
-    // l and r are 0-indexed
-    for (ll i=0;i<=q-1;i++)
-    {
-        ll id=(rand()%2)+1;
-
-        if (id==1)
-        {
-            ll l=(rand()%n);
-            ll r=(rand()%n);
-            // new_val is the new val of data point from indices l to r
-            ll new_val=(rand()%max_range)+1;
-            cout << id << " " << min(l,r) << " " << max(l,r) << " " << new_val << endl;   
-        }
-        else
-        {
-            ll l=(rand()%n);
-            ll r=(rand()%n);
-            cout << id << " " << min(l,r) << " " << max(l,r) << endl;
-        }
-    }
-
-    return 0;
+		if (type == 1) {
+			int l = rand(0, n - 2), r = rand(l, n - 1), inc = rand(1, 100);
+			cout << l << " " << r << " " << inc << endl;
+		}
+		else {
+			int l = rand(0, n - 2), r = rand(l, n - 1);
+			cout << l << " " << r << endl;
+		}
+	}
+	return 0;
 }
