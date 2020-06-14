52179ec31ceb1d78fd482e018618da997f6c9a3f
Kartikey Tewari <kartikeya30@gmail.com>
2020-06-14 23:51:39 +0530

    seg_tree corrected

--- seg_tree.cc
+++ seg_tree.cc
@@ -6,60 +6,6 @@
 #include <climits>
 using namespace std;
 
-
-// ===============================================
-void __print(int x) {cerr << x;}
-void __print(long x) {cerr << x;}
-void __print(float x) {cerr << x;}
-void __print(double x) {cerr << x;}
-void __print(unsigned x) {cerr << x;}
-void __print(long long x) {cerr << x;}
-void __print(long double x) {cerr << x;}
-void __print(unsigned long x) {cerr << x;}
-void __print(unsigned long long x) {cerr << x;}
-void __print(char x) {cerr << '\'' << x << '\'';}
-void __print(bool x) {cerr << (x ? "true" : "false");}
-void __print(const char *x) {cerr << '\"' << x << '\"';}
-void __print(const string &x) {cerr << '\"' << x << '\"';}
- 
-template<typename T, typename V>
-void __print(const pair<T, V> &x)
-{
-    cerr << '{'; __print(x.first);
-    cerr << ','; __print(x.second);
-    cerr << '}';
-}
- 
-template<typename T>
-void __print(const T &x)
-{
-    int f = 0;
-    cerr << '{';
-    for (auto &i : x)
-        cerr << (f++ ? "," : ""), __print(i);
-    cerr << "}";
-}
- 
-void _print()
-{
-    cerr << "]\n";
-}
- 
-template <typename T, typename... V>
-void _print(T t, V... v)
-{
-    __print(t);
-    if (sizeof...(v))
-        cerr << ", "; _print(v...);
-}
- 
-#ifndef ONLINE_JUDGE
-#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#else
-#define debug(x...)
-#endif
-// ===============================================
-
 int n, q;
 vector<int> arr;
 vector<int> tree;
@@ -143,7 +89,7 @@ int main()
             // query
             int l, r;
             cin >> l >> r;
-            cout << __min(l, r, 0, n - 1, 1) << endl;
+            cout << __min(l, r) << endl;
         }
         else
         {
@@ -151,7 +97,6 @@ int main()
             int index, val;
             cin >> index >> val;
             update(index, val);
-            debug(tree);
         }
     }
 
