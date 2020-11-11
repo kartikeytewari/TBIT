af647fb6cf895b6d03caa94a3fc9e471fd05cca4
Kartikey Tewari <kartikeya30@gmail.com>
2020-11-11 16:53:14 +0530

    updated graphs

--- data_dump/scripts/script_20.m
+++ data_dump/scripts/script_20.m
@@ -4,12 +4,12 @@ fen_tree=dlmread("../data/fen_tree_20_dw.txt"," ");
 
 n=brute(:,1);
 time_brute=brute(:,2);
-brute_val=plot(n,time_brute,";brute force algorithm;","linewidth", 2);
+brute_val=plot(n,time_brute,";Brute Force algorithm;","linewidth", 2);
 
 hold on
 n=seg_tree(:,1);
 time_seg=seg_tree(:,2);
-seg_val=plot(n,time_seg,";segment tree;", "linewidth", 2);
+seg_val=plot(n,time_seg,";Segment Tree;", "linewidth", 2);
 
 n=fen_tree(:,1);
 time_fen=fen_tree(:,2);
@@ -18,10 +18,8 @@ hold off
 
 x_val=xlabel("Sum of update and query operations");
 y_val=ylabel("Time in microseconds");
-legend("location","north");
+h=legend("location","northwest");
+set(h,"fontsize",22);
 set(x_val, "linewidth", 10, "fontsize", 22);
 set(y_val, "linewidth", 10, "fontsize", 22);
-set(gca, "linewidth", 6, "fontsize",34);
-
-h=legend({"Serial Wavellet Tree","Parallel Wavellet Tree"});
-set(h, "fontsize", 20);
+set(gca, "linewidth", 6, "fontsize",22);
