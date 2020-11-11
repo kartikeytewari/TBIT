ced7cb89e41b15d3ad5a43e6cec0eddf7be194bd
Kartikey Tewari <kartikeya30@gmail.com>
2020-11-11 15:56:24 +0530

    Changed font in graph scripts. Also added the graphs. Removed ofig graphs and inplace added the png images of graph.

--- data_dump/scripts/script_20.m
+++ data_dump/scripts/script_20.m
@@ -4,20 +4,24 @@ fen_tree=dlmread("../data/fen_tree_20_dw.txt"," ");
 
 n=brute(:,1);
 time_brute=brute(:,2);
-brute_val=plot(n,time_brute,";brute force algorithm;");
+brute_val=plot(n,time_brute,";brute force algorithm;","linewidth", 2);
 
 hold on
 n=seg_tree(:,1);
 time_seg=seg_tree(:,2);
-seg_val=plot(n,time_seg,";segment tree;");
+seg_val=plot(n,time_seg,";segment tree;", "linewidth", 2);
 
 n=fen_tree(:,1);
 time_fen=fen_tree(:,2);
-fen_val=plot(n,time_fen,";TBIT;");
+fen_val=plot(n,time_fen,";TBIT;", "linewidth", 2);
 hold off
 
 x_val=xlabel("Sum of update and query operations");
 y_val=ylabel("Time in microseconds");
-legend("location","northwest");
-set(x_val, "linewidth", 4, "fontsize", 20);
-set(y_val, "linewidth", 4, "fontsize", 20);
+legend("location","north");
+set(x_val, "linewidth", 10, "fontsize", 22);
+set(y_val, "linewidth", 10, "fontsize", 22);
+set(gca, "linewidth", 6, "fontsize",34);
+
+h=legend({"Serial Wavellet Tree","Parallel Wavellet Tree"});
+set(h, "fontsize", 20);
